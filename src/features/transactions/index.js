5282582f7a93b6c3f2f832247ba3e40bf67f3e55
berlianacahyarudiputri <berrlyanax8@gmail.com>
2023-06-25 21:20:20 +0700

    history

--- src/features/transactions/index.js
+++ src/features/transactions/index.js
@@ -77,18 +77,17 @@ function Transactions(){
     return(
         <>
             
-            <TitleCard title="Recent Transactions" topMargin="mt-2" TopSideButtons={<TopSideButtons applySearch={applySearch} applyFilter={applyFilter} removeFilter={removeFilter}/>}>
+            <TitleCard title="History" topMargin="mt-2" TopSideButtons={<TopSideButtons applySearch={applySearch} applyFilter={applyFilter} removeFilter={removeFilter}/>}>
 
                 {/* Team Member list in table format loaded constant */}
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Email Id</th>
-                        <th>Location</th>
-                        <th>Amount</th>
-                        <th>Transaction Date</th>
+                        <th>Mentee</th>
+                        <th>Mentor</th>
+                        <th>Durasi</th>
+                        <th>Tanggal</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -98,20 +97,15 @@ function Transactions(){
                                     <tr key={k}>
                                     <td>
                                         <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-circle w-12 h-12">
-                                                    <img src={l.avatar} alt="Avatar" />
-                                                </div>
-                                            </div>
+                                            
                                             <div>
-                                                <div className="font-bold">{l.name}</div>
+                                                <div className="font-bold">{l.mentee}</div>
                                             </div>
                                         </div>
                                     </td>
-                                    <td>{l.email}</td>
-                                    <td>{l.location}</td>
-                                    <td>${l.amount}</td>
-                                    <td>{moment(l.date).format("D MMM")}</td>
+                                    <td>{l.mentor}</td>
+                                    <td>{l.durasi}</td>
+                                    <td>{moment(l.tanggal).format("D MMM")}</td>
                                     </tr>
                                 )
                             })
